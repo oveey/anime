cb070b848bb803b16689552d4e08287f9cea5293
OveDev <ovieigere@gmail.com>
2023-08-25 02:45:04 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+/*=============== SHOW MENU ===============*/
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+/*=============== SCROLL HEADER ===============*/
+
+
+/*=============== BRAND SWIPER ===============*/
+var brandSwiper = new Swiper('.brand-swiper', {
+    spaceBetween: 24,
+    loop: true,
+    breakpoints: {
+        450: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+        },  
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+        },
+
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 24,
+        },
+        
+    },
+});
+
+/*=============== ANIME SWIPER ===============*/
+var animeSwiper = new Swiper('.anime-swiper', {
+    spaceBetween: 24,
+    loop: true,
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+        },  
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+        },
+
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+
+        1400: {
+            slidesPerView: 5,
+            spaceBetween: 24,
+        },
+        
+    },
+});
